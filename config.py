e68795e89d7f2a1eb6aadb5cc8f64e1d91335e6b
Юлия Кряжева <yulechka944992@gmail.com>
2026-09-02 01:19:36 +0900

    change files

--- config.py
+++ config.py
@@ -4,14 +4,22 @@ from configparser import ConfigParser
 def config(filename="database.ini", section="postgresql"):
     # create a parser
     parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception(
-            'Section {0} is not found in the {1} file.'.format(section, filename))
-    return db
+    # Пробуем разные кодировки
+    encodings = ['utf-8', 'utf-8-sig', 'cp1251', 'latin-1']
+
+    for encoding in encodings:
+        try:
+            parser.read(filename, encoding=encoding)
+            if parser.has_section(section):
+                db = {}
+                params = parser.items(section)
+                for param in params:
+                    db[param[0]] = param[1]
+                return db
+        except Exception:
+            continue
+
+    # Если ничего не сработало - ошибка
+    raise Exception(
+        f'Section {section} not found in {filename} or file encoding issue.'
+    )
